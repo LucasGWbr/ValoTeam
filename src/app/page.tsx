cad8bac0b92792f2eacf0e9a351d50f99121be16
Lucas Gustavo Weber <lucas.weber1@universo.univates.br>
2025-05-22 16:15:40 -0300

    favicon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,9 +13,9 @@ export default function Home() {
                 priority
             />
             <div className="z-10 w-full max-w-md overflow-hidden rounded-2xl border border-gray-100 shadow-xl">
-                <div className="flex flex-col items-center justify-center space-y-3 border-b border-gray-200 bg-gray-800 px-4 py-6 pt-8 text-center sm:px-16">
+                <div className="flex flex-col items-center justify-center space-y-3 border-b border-gray-200 bg-zinc-600 px-4 py-6 pt-8 text-center sm:px-16">
                     <h3 className="text-xl font-semibold">Login</h3>
-                    <p className="text-sm text-gray-500">
+                    <p className="text-sm ">
                         Use seu usuario e senha para acessar
                     </p>
                 </div>
