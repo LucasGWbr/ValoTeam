d25f18c3d478c541eb9536d6612e673a6672239f
Lucas Gustavo Weber <lucas.weber1@universo.univates.br>
2025-05-23 14:23:08 -0300

    Reformulação da tela de login e cadastro
    Telas de login e cadastro de usuario refeitas com mais atributos do tailwind a fim de melhorar usabilidade

--- src/app/cadastro/page.tsx
+++ src/app/cadastro/page.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import CryptoJS from "crypto-js";
 import { useState } from 'react';
 import {useRouter} from "next/navigation";
+import Link from "next/link";
 
 export default function cadastrarUser() {
     const router = useRouter();
@@ -29,37 +30,42 @@ export default function cadastrarUser() {
 
     return (
 
-        <div className="flex h-screen w-screen items-center justify-center">
+        <div className="flex h-screen w-screen items-center justify-end">
             <Image
                 src='/1249094.jpg'
                 alt="Fundo"
                 fill
                 quality={100}
-                style={{objectFit: 'cover'}}
+                style={{ objectFit: 'cover' }}
                 priority
             />
-            <div className="z-10 w-full max-w-md overflow-hidden rounded-2xl border border-gray-100 shadow-xl">
-                <div
-                    className="flex flex-col items-center justify-center space-y-3 border-b border-gray-200 bg-zinc-600 px-4 py-6 pt-8 text-center sm:px-16">
-                    <h3 className="text-xl font-semibold">Cadastro</h3>
-                    <p className="text-sm ">
-                        Crie uma conta com usuario e senha
+            <div className="mr-25 z-10 w-full max-w-xl h-screen overflow-hidden justify-content-center flex flex-col border border-gray-100 backdrop-blur-md bg-[#0F1923]/60">
+                <div className="w-full max-h-45 flex flex-col items-center justify-center space-y-3 px-4 py-6 pt-8 text-center sm:px-16">
+                    <h3 className="text-xl font-extrabold">CADASTRO</h3>
+                    <p className="text-sm font-medium">
+                        Crie uma conta agora!
                     </p>
                 </div>
-                <form className="flex justify-center p-3 gap-5 align-items-center backdrop-blur-sm bg-white/50"
-                      name="login" onSubmit={handleSubmit} method="post">
+                <form className="w-full p-3 gap-5 align-items-center" name="login" onSubmit={handleSubmit} method="post">
                     <div>
                         <div className="form-floating m-3">
-                            <input value={usuario} onChange={(e) => setUsuario(e.target.value)} required name="nome" type="text" className="form-control" placeholder="Nome"/>
+                            <input name="usuario" value={usuario} onChange={(e) => setUsuario(e.target.value)} required type="text" className="form-control" id="nome" placeholder="Nome"/>
                             <label htmlFor="nome">Usuario</label>
                         </div>
-                        <div className="form-floating m-3">
-                            <input value={senha} name="pass" onChange={(e) => setSenha(e.target.value)} required type="password" className="form-control" id="pass" placeholder="Senha"/>
+                        <div className="form-floating m-3 ">
+                            <input value={senha} onChange={(e) => setSenha(e.target.value)} required name="pass" type="password" className="form-control" id="pass" placeholder="Senha"/>
                             <label htmlFor="pass">Senha</label>
                         </div>
                     </div>
-                    <div>
-                        <button id="enviar" type="submit" className="btn btn-primary">Cadastrar</button>
+                    <div className="flex gap-4 justify-center items-center">
+                        <Link href="/">
+                            <button className="bg-[#E8E5DF] hover:bg-[#d6d3cd] text-[#0F1923] hover:text-black font-semibold py-2 px-6 rounded transition">
+                                VOLTAR
+                            </button>
+                        </Link>
+                        <button id="enviar" type="submit" className="bg-[#F23D58] hover:bg-[#d62e47] text-white font-semibold py-2 px-6 rounded transition">
+                            CRIAR CONTA
+                        </button>
                     </div>
                 </form>
             </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import {useState} from "react";
 import {useRouter} from "next/navigation";
 import CryptoJS from "crypto-js";
 import toast from 'react-hot-toast';
+import Link from "next/link";
 
 
 export default function Home() {
@@ -38,7 +39,7 @@ export default function Home() {
 
     return (
 
-        <div className="flex h-screen w-screen items-center justify-center">
+        <div className="flex h-screen w-screen items-center justify-end">
             <Image
                 src='/1249094.jpg'
                 alt="Fundo"
@@ -47,26 +48,33 @@ export default function Home() {
                 style={{ objectFit: 'cover' }}
                 priority
             />
-            <div className="z-10 w-full max-w-md overflow-hidden rounded-2xl border border-gray-100 shadow-xl">
-                <div className="flex flex-col items-center justify-center space-y-3 border-b border-gray-200 bg-zinc-600 px-4 py-6 pt-8 text-center sm:px-16">
-                    <h3 className="text-xl font-semibold">Login</h3>
-                    <p className="text-sm ">
-                        Use seu usuario e senha para acessar
+            <div className="mr-25 z-10 w-full max-w-xl h-screen overflow-hidden justify-content-center flex flex-col border border-gray-100 backdrop-blur-md bg-[#0F1923]/60">
+                <div className="w-full max-h-45 flex flex-col items-center justify-center space-y-3 px-4 py-6 pt-8 text-center sm:px-16">
+                    <h3 className="text-xl font-extrabold">LOGIN</h3>
+                    <p className="text-sm font-medium">
+                        Use seu usuario e senha para acessar!
                     </p>
                 </div>
-                <form className="flex justify-center p-3 gap-5 align-items-center backdrop-blur-sm bg-white/50" name="login" onSubmit={handleSubmit} method="post">
+                <form className="w-full p-3 gap-5 align-items-center" name="login" onSubmit={handleSubmit} method="post">
                     <div>
                         <div className="form-floating m-3">
                             <input name="usuario" value={user} onChange={(e) => setUser(e.target.value)} required type="text" className="form-control" id="nome" placeholder="Nome"/>
                             <label htmlFor="nome">Usuario</label>
                         </div>
-                        <div className="form-floating m-3">
+                        <div className="form-floating m-3 ">
                             <input value={senha} onChange={(e) => setSenha(e.target.value)} required name="pass" type="password" className="form-control" id="pass" placeholder="Senha"/>
                             <label htmlFor="pass">Senha</label>
                         </div>
                     </div>
-                    <div>
-                        <button id="enviar" type="submit" className="btn btn-primary">Login</button>
+                    <div className="flex gap-4 justify-center items-center">
+                        <Link href="/cadastro">
+                            <button className="bg-[#E8E5DF] hover:bg-[#d6d3cd] text-[#0F1923] hover:text-black font-semibold py-2 px-6 rounded transition">
+                                CRIAR CONTA
+                            </button>
+                        </Link>
+                        <button id="enviar" type="submit" className="bg-[#F23D58] hover:bg-[#d62e47] text-white font-semibold py-2 px-6 rounded transition">
+                            FAZER LOGIN
+                        </button>
                     </div>
                 </form>
             </div>
